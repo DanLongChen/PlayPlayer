a89b8ea5e03a8a1209973de290eb037ee856efeb
danlongchen <danlongchen@163.com>
2021-07-03 23:19:18 +0800

    优化配置

---
+++ app/src/main/java/com/chiron/playpalyer/openGL/MyGLSurfaceView.java
@@ -0,0 +1,9 @@
+package com.chiron.playpalyer.openGL;
+
+import android.opengl.GLSurfaceView;
+
+//public class MyGLSurfaceView extends GLSurfaceView {
+//    private static final String TAG = "MyGLSurfaceView";
+//
+//    private
+//}
